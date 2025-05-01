998fd6535c26c5ab2b6bc296dad322d3dab2a0c7
jahid45780 <jahidhossain88899@gmail.com>
2025-05-01 23:23:16 +0600

    add the ts

---
+++ module/src/alias.ts
@@ -0,0 +1,28 @@
+{
+
+    type student ={
+        Firstname:string,
+        roll:number,
+        address:string,
+        phoneNunber ? :string
+        
+    }
+
+
+    const student1 : student ={
+
+        Firstname:"jahid",
+          roll:99,
+          address:'ctg',
+
+    } 
+
+    const student2 : student ={
+
+        Firstname:"korim",
+          roll:909,
+          address:'Dha',
+
+    } 
+
+}

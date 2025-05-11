44c19d1345d84454dc0aee2fbade80e66b23b95b
jahid45780 <jahidhossain88899@gmail.com>
2025-05-11 22:26:32 +0600

    add the Abstraction.ts

--- module3/Abstraction.ts
+++ module3/Abstraction.ts
@@ -1,7 +1,63 @@
 {
 
 
-    
+ // idea
+ 
+ interface vehicle1{
+     startEngine(): void
+     stopEngine():void
+     movie():void
+ }
+
+ // real impremention 
+
+ class Car implements vehicle1{
+     startEngine(): void {
+         console.log(`iam start the car engine`);
+     }
+
+     stopEngine(): void {
+         console.log(`iam stopping the car engine`);
+     }
+
+     movie(): void {
+         console.log(`iam move the ctg in the car`);
+     }
+ }
+
+ const toyotaCar = new Car()
+ toyotaCar.startEngine()
+ console.log(toyotaCar);
+
+// class Abstraction
+
+
+ // real impremention 
+
+
+
+  abstract class Car2 {
+    abstract startEngine(): void
+
+    abstract stopEngine(): void 
+
+    abstract movie(): void 
+ }
+
+ class toyotaCar2 extends Car2{
+    startEngine(): void {
+        console.log(`the car now start`);
+    }
+
+    stopEngine(): void {
+        console.log(`the car now stop`);
+    }
+
+    movie(): void {
+        console.log(` the car now moved the place `);
+    }
+ }
+
 
 
 
